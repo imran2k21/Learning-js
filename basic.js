34d44738742ac58c7da19c1bf3261cd5cb6e6869
imran2k21 <bdsavar2@gmail.com>
2021-09-10 04:10:48 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,99 @@
+// var studentName = "imran";
+// var age = 21;
+// var imran = true;
+
+// console.log(imran);
+// ####################
+
+
+
+// var eggPrice = 32;
+// var bannaPrice = 12;
+
+// var sum = eggPrice + bannaPrice;
+// var difference = eggPrice - bannaPrice;
+// var multiplication = eggPrice * bannaPrice;
+// var division = eggPrice / bannaPrice;
+
+// total = eggPrice + bannaPrice - eggPrice * bannaPrice / eggPrice;
+
+// console.log(total);
+// ####################
+
+
+
+// eggPrice = 32 + 10;
+// bannaPrice = 12 - 4;
+
+// console.log(eggPrice);
+// ####################
+
+
+// eggPrice = 32 * 2;
+// bannaPrice = 12 / 2;
+
+// console.log(eggPrice);
+// ####################
+
+
+
+// var age = 21;
+// age = 21 + 1;
+
+// age += 1;
+// age ++;
+
+// console.log(age);
+// ####################
+
+
+// var firstName = "Imran";
+// var lastName = "Hosen";
+
+// var fullName = firstName + ' ' + lastName;
+
+// console.log(fullName);
+// ####################
+
+
+
+// var eggPrice = "20";
+// var bannaPrice = "21";
+// var total = eggPrice + bannaPrice;
+
+
+// console.log(total);
+// ####################
+
+
+
+// var eggPrice = "32.50";
+// var eggPriceList = parseInt(eggPrice);
+
+// var bannaPrice = 12.50;
+// var bannaPriceList = parseFloat(bannaPrice);
+
+
+// console.log(eggPrice);
+// ####################
+
+
+
+// var tshirtPrice1 = "200";
+// var tshirtPrice2 = 200;
+// var tshirtPrice3 = true;
+
+// console.log(typeof tshirtPrice3);
+// ####################
+
+
+// var mango = 7;
+// var hungry = 2;
+
+// var remaining = mango % hungry;
+
+
+// console.log(remaining);
+// ####################
+
+
